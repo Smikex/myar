371e85c9e55e860fd9a4fab4071dbf4e334f89ad
Your Name <you@example.com>
2018-06-08 14:31:43 +0300

    first commit

---
+++ Wp/WP_FicusLife_Theme/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="RU">
+<head>
+	<meta charset="utf-8">
+	<title><?= wp_title(); ?></title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body>
+	
---
+++ Верстка/js/common.js
@@ -0,0 +1,78 @@
+var mobileBtn = $("#mobileBtn");
+mobileBtn.on('click',  function(event) {
+	event.preventDefault();
+	$(this).toggleClass('active');
+	$(".menuTop").toggleClass('active');
+});
+
+var sliderServices = $(".sliderServices");
+sliderServices.slick({
+	slidesToShow: 4,
+	arrows: true,
+	dots: false,
+	speed: 1000,
+	autoplaySpeed: 4000,
+	infinite: false,
+	nextArrow: '<div class="icon-arrow icon-arrow-next"><img src="img/arrow_slider_next.png" alt="" /></div>',
+	prevArrow: '<div class="icon-arrow icon-arrow-prev"><img src="img/arrow_slider_prev.png" alt="" /></div>',
+	appendArrows: $(".sliderServices__nav"),
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 651,
+			settings: {
+				slidesToShow: 1,
+			}
+		},
+
+	],
+});
+
+// var sliderReality = $(".sliderReality");
+// sliderReality.slick({
+// 	slidesToShow: 4,
+// 	arrows: true,
+// 	dots: false,
+// 	speed: 1000,
+// 	autoplaySpeed: 4000,
+// 	infinite: false,
+// 	nextArrow: '<div class="icon-arrow icon-arrow-next"><img src="img/arrow_slider_next.png" alt="" /></div>',
+// 	prevArrow: '<div class="icon-arrow icon-arrow-prev"><img src="img/arrow_slider_prev.png" alt="" /></div>',
+// 	appendArrows: $(".sliderReality__nav"),
+// 	responsive: [
+// 		{
+// 			breakpoint: 1200,
+// 			settings: {
+// 				slidesToShow: 3,
+// 			}
+// 		},
+
+// 		{
+// 			breakpoint: 992,
+// 			settings: {
+// 				slidesToShow: 2,
+// 			}
+// 		},
+// 		{
+// 			breakpoint: 651,
+// 			settings: {
+// 				slidesToShow: 1,
+// 			}
+// 		},
+
+// 	],
+// });
+// $(document).ready(function(){	
+// });
